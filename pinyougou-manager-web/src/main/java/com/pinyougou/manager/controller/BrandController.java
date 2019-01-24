b128e0bc771cec9b67118770d5e0266ced625549
gengbin11 <gengbin11@qq.com>
2019-01-24 22:25:13 +0800

    NO.2 the CRUD about the brandservice

--- pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -2,6 +2,7 @@ package com.pinyougou.manager.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,14 +10,102 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
 
+import entity.PageResult;
+import entity.Result;
+
 @RestController
 @RequestMapping("/brand")
 public class BrandController {
 	@Reference
 	private BrandService brandService;
 	
+	/**
+	 * 根据条件查询
+	 * @param brand
+	 * @param page
+	 * @param rows
+	 * @return
+	 */
+	@RequestMapping("/search")
+	public PageResult search(@RequestBody TbBrand brand,int page,int rows){
+		return brandService.findPage(brand, page, rows);
+	}
+
 	@RequestMapping("/findAll")
-	public List<TbBrand> findAll(){
-		return brandService.findAll(); 
+	public List<TbBrand> findAll() {
+		return brandService.findAll();
+	}
+
+	/**
+	 * 返回全部列表
+	 * 
+	 * @param page
+	 *            当前页
+	 * @param rows
+	 *            每页列数
+	 * @return
+	 */
+	@RequestMapping("/findPage")
+	public PageResult findPage(int page, int rows) {
+		return brandService.findPage(page, rows);
+	}
+
+	/**
+	 * 增加品牌
+	 * 
+	 * @param brand
+	 * @return
+	 */
+	@RequestMapping("/add")
+	public Result add(@RequestBody TbBrand brand) {
+		try {
+			brandService.add(brand);
+			return new Result(true, "增加成功");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Result(false, "增加失败");
+		}
+	}
+
+	/**
+	 * 更新品牌
+	 * @param brand
+	 * @return
+	 */
+	@RequestMapping("/update")
+	public Result update(@RequestBody TbBrand brand) {
+		try {
+			brandService.update(brand);
+			return new Result(true, "修改成功");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Result(false, "修改失败");
+		}
+	}
+	
+	/**
+	 * 获取实体
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping("/findOne")
+	public TbBrand findOne(Long id){
+		return brandService.findOne(id);
+	}
+	
+	/**
+	 * 批量删除
+	 * @param ids
+	 * @return
+	 */
+	@RequestMapping("/delete")
+	public Result delete(Long[] ids){
+		try {
+			brandService.delete(ids);
+			return new Result(true, "删除成功");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Result(false, "删除失败");
+		}
 	}
 }
--- pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
+++ pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -4,16 +4,69 @@ import java.util.List;
 
 import com.pinyougou.pojo.TbBrand;
 
+import entity.PageResult;
+
 /**
  * 品牌服务层接口
- * @author 耿宾
+ * 
+ * @author gengbin
  *
  */
-
 public interface BrandService {
 	/**
+	 * 根据除”每页条数“和”当前页数“之外的变量查询的分页方法
+	 * 
+	 * @param brand
+	 * @param pageNum当前页码
+	 * @param pageSize每页记录数
+	 * @return
+	 */
+	public PageResult findPage(TbBrand brand, int pageNum, int pageSize);
+
+	/**
+	 * 批量删除
+	 * 
+	 * @param ids
+	 */
+	public void delete(Long[] ids);
+
+	/**
 	 * 返回全部列表
+	 * 
 	 * @return
 	 */
 	public List<TbBrand> findAll();
+
+	/**
+	 * 返回分页列表
+	 * 
+	 * @param pageNum
+	 *            当前页面
+	 * @param pageSize
+	 *            每页记录数
+	 * @return
+	 */
+	public PageResult findPage(int pageNum, int pageSize);
+
+	/**
+	 * 增加
+	 * 
+	 * @param brand
+	 */
+	public void add(TbBrand brand);
+
+	/**
+	 * 修改
+	 * 
+	 * @param brand
+	 */
+	public void update(TbBrand brand);
+
+	/**
+	 * 根据id获取实体
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public TbBrand findOne(Long id);
 }
