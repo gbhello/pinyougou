b63edf2397c8acb668d16da764194a751e51a0a5
gengbin11 <gengbin11@qq.com>
2019-03-17 16:27:51 +0800

    网页静态化基本实现

--- pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/GoodsController.java
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/GoodsController.java
@@ -1,5 +1,7 @@
 package com.pinyougou.manager.controller;
+
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.pinyougou.page.service.ItemPageService;
 import com.pinyougou.pojo.TbGoods;
 import com.pinyougou.pojo.TbItem;
 import com.pinyougou.pojogroup.Goods;
@@ -13,112 +15,133 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
+
 /**
  * controller
- * @author Administrator
  *
+ * @author Administrator
  */
 @RestController
 @RequestMapping("/goods")
 public class GoodsController {
 
-	@Reference
-	private GoodsService goodsService;
-	@Reference
-	private ItemSearchService itemSearchService;
-	
-	/**
-	 * 返回全部列表
-	 * @return
-	 */
-	@RequestMapping("/findAll")
-	public List<TbGoods> findAll(){			
-		return goodsService.findAll();
-	}
-	
-	
-	/**
-	 * 返回全部列表
-	 * @return
-	 */
-	@RequestMapping("/findPage")
-	public PageResult  findPage(int page,int rows){			
-		return goodsService.findPage(page, rows);
-	}
-	
-	/**
-	 * 修改
-	 * @param goods
-	 * @return
-	 */
-	@RequestMapping("/update")
-	public Result update(@RequestBody Goods goods){
-		try {
-			goodsService.update(goods);
-			return new Result(true, "修改成功");
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new Result(false, "修改失败");
-		}
-	}	
-	
-	/**
-	 * 获取实体
-	 * @param id
-	 * @return
-	 */
-	@RequestMapping("/findOne")
-	public Goods findOne(Long id){
-		return goodsService.findOne(id);		
-	}
-	
-	/**
-	 * 批量删除
-	 * @param ids
-	 * @return
-	 */
-	@RequestMapping("/delete")
-	public Result delete(Long [] ids){
-		try {
-			goodsService.delete(ids);
-			itemSearchService.deleteByGoodsIds(Arrays.asList(ids));
-			return new Result(true, "删除成功"); 
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new Result(false, "删除失败");
-		}
-	}
-	
-		/**
-	 * 查询+分页
-	 * @param goods
-	 * @param page
-	 * @param rows
-	 * @return
-	 */
-	@RequestMapping("/search")
-	public PageResult search(@RequestBody TbGoods goods, int page, int rows  ){
-		return goodsService.findPage(goods, page, rows);		
-	}
-	
-	@RequestMapping("/updateStatus")
-	public Result updateStatus(Long[] ids,String status){
-		try {
-			goodsService.updateStatus(ids, status);
-			//按照SPU ID查询SKU列表（状态为1）
-			if(status.equals("1")){//审核通过
-				List<TbItem> itemList = goodsService.findItemListByGoodsIdandStatus(ids, status);
-				//调用搜索接口实现数据批量导入
-				if(itemList.size()>0){
-					itemSearchService.importList(itemList);
-				}else{
-					System.out.println("没有明细数据");
-				}
-			}
-			return new Result(true, "更新成功");
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new Result(false, "更新失败");
-		}
-	}
+    @Reference
+    private GoodsService goodsService;
+    @Reference
+    private ItemSearchService itemSearchService;
+
+    /**
+     * 返回全部列表
+     *
+     * @return
+     */
+    @RequestMapping("/findAll")
+    public List<TbGoods> findAll() {
+        return goodsService.findAll();
+    }
+
+
+    /**
+     * 返回全部列表
+     *
+     * @return
+     */
+    @RequestMapping("/findPage")
+    public PageResult findPage(int page, int rows) {
+        return goodsService.findPage(page, rows);
+    }
+
+    /**
+     * 修改
+     *
+     * @param goods
+     * @return
+     */
+    @RequestMapping("/update")
+    public Result update(@RequestBody Goods goods) {
+        try {
+            goodsService.update(goods);
+            return new Result(true, "修改成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false, "修改失败");
+        }
+    }
+
+    /**
+     * 获取实体
+     *
+     * @param id
+     * @return
+     */
+    @RequestMapping("/findOne")
+    public Goods findOne(Long id) {
+        return goodsService.findOne(id);
+    }
+
+    /**
+     * 批量删除
+     *
+     * @param ids
+     * @return
+     */
+    @RequestMapping("/delete")
+    public Result delete(Long[] ids) {
+        try {
+            goodsService.delete(ids);
+            itemSearchService.deleteByGoodsIds(Arrays.asList(ids));
+            return new Result(true, "删除成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false, "删除失败");
+        }
+    }
+
+    /**
+     * 查询+分页
+     *
+     * @param goods
+     * @param page
+     * @param rows
+     * @return
+     */
+    @RequestMapping("/search")
+    public PageResult search(@RequestBody TbGoods goods, int page, int rows) {
+        return goodsService.findPage(goods, page, rows);
+    }
+
+    @RequestMapping("/updateStatus")
+    public Result updateStatus(Long[] ids, String status) {
+        try {
+            goodsService.updateStatus(ids, status);
+            //按照SPU ID查询SKU列表（状态为1）
+            if (status.equals("1")) {//审核通过
+                List<TbItem> itemList = goodsService.findItemListByGoodsIdandStatus(ids, status);
+                //调用搜索接口实现数据批量导入
+                if (itemList.size() > 0) {
+                    itemSearchService.importList(itemList);
+                } else {
+                    System.out.println("没有明细数据");
+                }
+                //静态页生成
+                for (Long goodsIs :
+                        ids) {
+                    itemPageService.genItemHtml(goodsIs);
+                }
+            }
+            return new Result(true, "更新成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false, "更新失败");
+        }
+    }
+
+    @Reference(timeout = 600000)
+    private ItemPageService itemPageService;
+
+    @RequestMapping("/genHtml")
+    public void genHtml(Long goodsId) {
+        itemPageService.genItemHtml(goodsId);
+    }
+
 }
